d71ad9c5e5c13c3131caa2d0b9c180b9621fd9de
Lee Shao Wee <lee_shao_wee@hotmail.com>
2021-06-05 10:37:32 +0800

    add build file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("src/data/db.json");
-const middlewares = jsonServer.defaults({});
+const middlewares = jsonServer.defaults({
+  static: "./build",
+});
 const port = process.env.PORT || 5000;
 
 server.use(middlewares);
