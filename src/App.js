a38f501e830a263e6cf86ea2be70853b4540de9a
shao456 <lee_shao_wee@hotmail.com>
2021-07-08 20:16:38 +0800

    added light and dark mode theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import { allBlogs } from "data/db.json";
 import HeaderNav from "./components/HeaderNav";
 import Create from "./components/Create";
@@ -6,6 +6,8 @@ import BlogDetails from "./components/BlogDetails";
 import Main from "components/Main";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
+
+export const AppContext = createContext();
 
 const App = () => {
   let storedBlogs = localStorage.getItem("blogs");
@@ -16,24 +18,40 @@ const App = () => {
 
   const [blogs, setBlogs] = useState(JSON.parse(storedBlogs));
 
+  const [darkMode, setDarkMode] = useState(false);
+  const white = "rgb(253, 239, 239)";
+  const black = "rgba(0, 0, 0, 0.8)";
+  const bgColor = darkMode ? black : white;
+  const fontColor = darkMode ? white : black;
+
+  const appStyle = {
+    backgroundColor: bgColor,
+    color: fontColor,
+    minHeight: "100vh",
+  };
+
   return (
-    <Router>
-      <HeaderNav />
-      <Switch>
-        <Route path="/" exact>
-          <Main homePage={true} blogs={blogs} setBlogs={setBlogs} />
-        </Route>
-        <Route path="/create">
-          <Create blogs={blogs} setBlogs={setBlogs} />
-        </Route>
-        <Route path="/bookmark">
-          <Main blogs={blogs} setBlogs={setBlogs} />
-        </Route>
-        <Route path="/blogs/:id">
-          <BlogDetails blogs={blogs} setBlogs={setBlogs} />
-        </Route>
-      </Switch>
-    </Router>
+    <AppContext.Provider value={{ darkMode, setDarkMode }}>
+      <div style={appStyle}>
+        <Router>
+          <HeaderNav />
+          <Switch>
+            <Route path="/" exact>
+              <Main homePage={true} blogs={blogs} setBlogs={setBlogs} />
+            </Route>
+            <Route path="/create">
+              <Create blogs={blogs} setBlogs={setBlogs} />
+            </Route>
+            <Route path="/bookmark">
+              <Main blogs={blogs} setBlogs={setBlogs} />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails blogs={blogs} setBlogs={setBlogs} />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </AppContext.Provider>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,25 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AppContext } from "App";
 
 const Navbar = () => {
+  const { darkMode, setDarkMode } = useContext(AppContext);
+
   return (
     <nav>
       <h1>Shao Blog</h1>
       <ul>
+        <li>
+          <input
+            id="mode"
+            type="checkbox"
+            className="switch"
+            onChange={(e) => setDarkMode(e.target.checked)}
+          />
+          <label htmlFor="mode">
+            <i className={`far fa-${darkMode ? "sun" : "moon"}`}></i>
+          </label>
+        </li>
         <li>
           <Link to="/">Home</Link>
         </li>
